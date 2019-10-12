e0406528160cf4e9b2d80dc4b668beac4731c03d
sogreshilin <sogreshilin@yandex-team.ru>
2019-10-12 18:08:44 +0700

    lab4: Реализация задачи №4

---
+++ lab4/project.clj
@@ -0,0 +1,5 @@
+(defproject lab4 "0.1.0-SNAPSHOT"
+  :description "Лабораторная №4 по курсу Современные методы программирования"
+  :url "http://ccfit.nsu.ru/~shadow/DT6/tasks/clojure1.html"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns lab4.generator})
