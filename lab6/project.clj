5d93a6a44ec997b559b0e795faa9e9ed880a4ce1
sogreshilin <sogreshilin@yandex-team.ru>
2019-11-03 11:23:09 +0700

    lab6: initial commit

---
+++ lab6/project.clj
@@ -0,0 +1,5 @@
+(defproject lab6 "0.1.0-SNAPSHOT"
+  :description "Лабораторная №6 по курсу Современные методы программирования"
+  :url "http://ccfit.nsu.ru/~shadow/DT6/tasks/clojure3-1.html"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns lab6.integral})
